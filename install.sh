35ebf822f1a11d175384a59bfe4ed5bab26ff658
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-30 12:19:53 +0100

    Fix: Install systemd unit file to the correct place

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 prefix="/usr/local"
 bindir="${prefix}/bin"
 datadir="${prefix}/share"
-libdir="${prefix}/lib"
+sysconfdir="/etc"
 
 install -Dm 755 ffstream "${bindir}/ffstream"
-install -Dm 644 ffstream.service "${libdir}/systemd/system/ffstream.service"
+install -Dm 644 ffstream.service "${sysconfdir}/systemd/system/ffstream.service"
 install -Dm 644 ffstream.conf.example "${datadir}/ffstream/ffstream.conf.example"
